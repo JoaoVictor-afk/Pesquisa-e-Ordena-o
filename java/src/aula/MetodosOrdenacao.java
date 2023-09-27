7beedd63ebd66667d02d6eb666b4220dd80658d5
Your Name <you@example.com>
2023-09-27 11:01:49 -0300

    ola

--- java/src/aula/MetodosOrdenacao.java
+++ java/src/aula/MetodosOrdenacao.java
@@ -65,59 +65,70 @@ public class MetodosOrdenacao {
         } while (distancia > 1 || houveTroca);
     }
     
- public static void intercala(ArrayList<Integer> lista, long   n) {
-       	int   meio;
-       	int  i, j, k;
-       	ArrayList<Integer> vetorTemporario =new ArrayList<Integer>();
-
-
-     	meio = (int)(n / 2);
-
-     	i = 0; //indice da porcao esquerda
-     	j = meio; //indice da porcao direita
-     	k = 0; //indice do vetor temporario
-
-    	while (i < meio && j < n) {
-     		if (lista.get(i) < lista.get(j)) {
-     			vetorTemporario.add(lista.get(i),k);  
-    	  		++i;
-     		} else {
-     			vetorTemporario.add(lista.get(j),k); 
-    	  		++j;
-     		}
-     		++k;
-    	}
-
-    	if (i == meio) {
-     		while (j < n) { //nao hah mais elementos na porcao esquerda
-     			vetorTemporario.add(lista.get(j),k); 
-    	  		++j;
-     	  		++k;
-     		}
-     	} else {
-     		while (i < meio) {
-     			vetorTemporario.add(lista.get(i),k);  
-     	  		++i;
-    	    	++k;
-     		}
-     	}
-
-     	for (i = 0; i < n; ++i) {
-    		lista.add(i,vetorTemporario.get(i));
-     	}
-
-     	vetorTemporario.clear();
- }
-
-     public static void mergeSort(ArrayList<Integer> lista, long n) { //responsavel pela divisao = recursao
-    	 int meio;
-
-         if (n > 1) {
-            meio = (int)(n / 2);
-
-            mergeSort(lista, meio); //porcao da esquerda
-            mergeSort(lista, n - meio); //porcao da direita
-             intercala(lista, n);
-      }
+   public void merge(int arr[], int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+ 
+        // Create temp arrays
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+ 
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+ 
+        // Merge the temp arrays
+ 
+        // Initial indices of first and second subarrays
+        int i = 0, j = 0;
+ 
+        // Initial index of merged subarray array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+ 
+        // Copy remaining elements of L[] if any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+ 
+        // Copy remaining elements of R[] if any
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+ 
+    
+   public void sort(int arr[], int l, int r)
+    {
+        if (l < r) {
+ 
+            // Find the middle point
+            int m = l + (r - l) / 2;
+ 
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+ 
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
     }
 }
